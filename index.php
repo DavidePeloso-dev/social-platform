2c2950db150c388246005de1ef02746d9615bf47
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-04-25 17:36:19 +0200

    Print database data

--- index.php
+++ index.php
@@ -11,26 +11,17 @@ if ($connection && $connection->connect_error) {
 }
 
 $sql =
-    "SELECT `posts`.`id` AS `post_id`,`posts`.`title` AS `post_title`, COUNT(`likes`.`post_id`) AS `post_likes`
-FROM `posts`
-LEFT JOIN `likes`
-ON `likes`.`post_id` = `posts`.`id`
-GROUP BY `posts`.`id`  
-ORDER BY `post_likes`  ASC";
+    "SELECT DISTINCT `posts`.`title`, `posts`.`tags`
+    FROM `posts`
+    LEFT JOIN `likes`
+    ON `likes`.`post_id` = `posts`.`id`
+    JOIN `media_post`
+    ON `media_post`.`post_id` = `posts`.`id`
+    JOIN `medias`
+    ON `medias`.`id` = `media_post`.`media_id`
+    WHERE `likes`.`post_id` IS NULL";
 
 $result = $connection->query($sql);
-var_dump($result);
-
-if ($result && $result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        var_dump($row);
-    };
-} elseif ($result) {
-    echo "0 result";
-} else {
-    echo "query error";
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -40,11 +31,45 @@ if ($result && $result->num_rows > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN' crossorigin='anonymous'>
 </head>
 
 <body>
+    <div class="container">
+        <h1>Less liked posts</h1>
+        <div class="row row-cols-3">
+            <?php if ($result && $result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) :
+                    $title = $row['title'];
+                    $tags = json_decode($row['tags']); ?>
 
+                    <div class="col p-3">
+                        <div class="card">
+                            <img src="https://picsum.photos/400?random=1" class="card-img-top" alt="...">
+                            <div class="card-body position-relative">
+                                <h5 class="card-title"><?= $title ?></h5>
+                                <div class="card-text mb-5">
+                                    <span class="mb-0">Tags:</span>
+                                    <?php foreach ($tags as $tag) : ?>
+                                        <div class="badge text-bg-secondary <?= $tag ?>"><?= $tag ?></div>
+                                    <?php endforeach ?>
+                                </div>
+                                <a href="#" class="btn btn-primary position-absolute bottom-0 end-0 mb-3 me-3">Read More</a>
+                            </div>
+                        </div>
+                    </div>
 
+            <?php endwhile;
+            } elseif ($result) {
+                echo "0 result";
+            } else {
+                echo "query error";
+            }; ?>
+
+        </div>
+    </div>
+
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL' crossorigin='anonymous'></script>
 </body>
 
 </html>
