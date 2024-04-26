d2c9809742f0b1d1f321c6750b341995a4d593cf
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-04-26 18:22:51 +0200

    Add images and use class post for print them dynamically from db

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+require_once __DIR__ . "/models/post.php";
+require_once __DIR__ . "/models/media.php";
+
 define("DB_SERVERNAME", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "root");
@@ -11,17 +15,24 @@ if ($connection && $connection->connect_error) {
 }
 
 $sql =
-    "SELECT DISTINCT `posts`.`title`, `posts`.`tags`
-    FROM `posts`
-    LEFT JOIN `likes`
-    ON `likes`.`post_id` = `posts`.`id`
-    JOIN `media_post`
-    ON `media_post`.`post_id` = `posts`.`id`
-    JOIN `medias`
-    ON `medias`.`id` = `media_post`.`media_id`
-    WHERE `likes`.`post_id` IS NULL";
+    "SELECT DISTINCT `posts`.`id`,`posts`.`title`, `posts`.`tags`,JSON_ARRAYAGG(`medias`.`path`) AS `medias`, COUNT(`medias`.`path`) as media_count FROM `posts` LEFT JOIN `likes` ON `likes`.`post_id` = `posts`.`id` JOIN `media_post` ON `media_post`.`post_id` = `posts`.`id` JOIN `medias` ON `medias`.`id` = `media_post`.`media_id` WHERE `likes`.`post_id` IS NULL GROUP BY `posts`.`id`";
 
 $result = $connection->query($sql);
+/* var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc());
+var_dump($result->fetch_assoc()); */
+
+$posts = [];
 ?>
 
 <!DOCTYPE html>
@@ -39,33 +50,50 @@ $result = $connection->query($sql);
         <h1>Less liked posts</h1>
         <div class="row row-cols-3">
             <?php if ($result && $result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) :
-                    $title = $row['title'];
-                    $tags = json_decode($row['tags']); ?>
-
-                    <div class="col p-3">
-                        <div class="card">
-                            <img src="https://picsum.photos/400?random=1" class="card-img-top" alt="...">
-                            <div class="card-body position-relative">
-                                <h5 class="card-title"><?= $title ?></h5>
-                                <div class="card-text mb-5">
-                                    <span class="mb-0">Tags:</span>
-                                    <?php foreach ($tags as $tag) : ?>
-                                        <div class="badge text-bg-secondary <?= $tag ?>"><?= $tag ?></div>
-                                    <?php endforeach ?>
-                                </div>
-                                <a href="#" class="btn btn-primary position-absolute bottom-0 end-0 mb-3 me-3">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-
-            <?php endwhile;
+                while ($row = $result->fetch_assoc()) {
+                    $tags = json_decode($row['tags']);
+                    $medias = json_decode($row['medias']);
+                    $post = new post($row['id'], $row['title'], $tags, $medias);
+                    array_push($posts, $post);
+                }
             } elseif ($result) {
                 echo "0 result";
             } else {
                 echo "query error";
-            }; ?>
-
+            };
+            foreach ($posts as $post) : ?>
+                <div class="col p-3">
+                    <div class="card">
+                        <div id="carouselExampleAutoplaying<?= $post->id ?>" class="carousel slide card-img-top" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <?php foreach ($post->medias as $i => $media) : ?>
+                                    <div class="carousel-item <?php if ($i == 0) echo 'active' ?>">
+                                        <img style="width: 100%; aspect-ratio: 1;" src="./<?= $media ?>" class="d-block w-100" alt="<?= $post->title ?>">
+                                    </div>
+                                <?php endforeach ?>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying<?= $post->id ?>" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying<?= $post->id ?>" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <div class="card-body position-relative">
+                            <h5 class="card-title"><?= $post->title ?></h5>
+                            <div class="card-text mb-5">
+                                <span class="mb-0">Tags:</span>
+                                <?php foreach ($post->tags as $tag) : ?>
+                                    <div class="badge text-bg-secondary"><?= $tag ?></div>
+                                <?php endforeach ?>
+                            </div>
+                            <a href="#" class="btn btn-primary position-absolute bottom-0 end-0 mb-3 me-3">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach ?>
         </div>
     </div>
 
